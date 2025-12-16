1fbac855610ab1ea1844bc79c28e76d5bbf48666
datavorous <datavorous.work@gmail.com>
2025-12-16 13:28:13 +0530

    feat: added moving average observer

---
+++ inwhale/observers/moving_average.py
@@ -0,0 +1,25 @@
+from .base import Observer
+
+
+class MovingAverageObserver(Observer):
+    def __init__(self, momentum=0.1):
+        super().__init__()
+        self.momentum = momentum
+        self.min_val = None
+        self.max_val = None
+
+    def observe(self, x):
+        min_x = x.min()
+        max_x = x.max()
+
+        if self.min_val is None or self.max_val is None:
+            self.min_val = min_x
+            self.max_val = max_x
+        else:
+            self.min_val = (1 - self.momentum) * self.min_val + self.momentum * min_x
+            self.max_val = (1 - self.momentum) * self.max_val + self.momentum * max_x
+
+    def get_range(self):
+        if self.min_val is None:
+            raise RuntimeError("No data observed yet.")
+        return self.min_val, self.max_val
