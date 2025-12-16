9f75d17ee27e6fb435fa3a7e85e1d8a2874fdbb6
datavorous <datavorous.work@gmail.com>
2025-12-16 19:30:07 +0530

    docs: added docstrings for round away with zero RS

--- inwhale/rounding/round_away_zero.py
+++ inwhale/rounding/round_away_zero.py
@@ -4,5 +4,16 @@ from .base import RoundingStrategy
 
 
 class RoundAwayFromZero(RoundingStrategy):
+    """
+    This rounding straegy rounds values away from zero, by computing
+    sign(x) * ceil(abs(x)).
+    Positive values round up, negative values round down.
+    """
     def round(self, x):
+        """
+        Docstring for round
+        
+        :param x: inoyt tensir of numeric values
+        :return: tensor with values rounded away from zero
+        """
         return torch.sign(x) * torch.ceil(torch.abs(x))
