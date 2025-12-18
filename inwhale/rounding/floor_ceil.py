5b016698ce9f70b2b2cafebe3d0fc7209586c87e
datavorous <datavorous.work@gmail.com>
2025-12-18 13:39:35 +0530

    docs: added docstrings for floor and ceil rounding classes

--- inwhale/rounding/floor_ceil.py
+++ inwhale/rounding/floor_ceil.py
@@ -4,10 +4,20 @@ from .base import RoundingStrategy
 
 
 class FloorRounding(RoundingStrategy):
+    """
+    Rounds the input tensor down to the nearest integer using the floor function.
+    param x: Input tensor to be rounded down.
+    return: Tensor rounded down to the nearest integer.
+    """
     def round(self, x):
         return torch.floor(x)
 
 
 class CeilRounding(RoundingStrategy):
+    """
+    Rounds the input tensor up to the nearest integer using the ceil function.
+    param x: Input tensor to be rounded up.
+    return: Tensor rounded up to the nearest integer.
+    """
     def round(self, x):
         return torch.ceil(x)
